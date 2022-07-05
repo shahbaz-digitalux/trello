57378be094a63dd9de8d65e065666d7263580b0d
Shahbaz <shahbaz.hassan@digitalux.pk>
2022-07-05 21:45:28 +0500

    created cars show add functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,12 @@
-const Card=({name})=>(
-    <div className="flex justify-between items-center bg-[#ffffff] rounded-[3px] text-[#5e6c84] py-[1px] px-[8px] cursor-pointer hover:bg-[#f4f5f7] shadow-[0_1px_0_#091e4240]">
+const Card = ({name, setCards}) => (
+    <div
+        className="mb-[5px] flex justify-between items-center bg-[#ffffff] rounded-[3px] text-[#5e6c84] py-[1px] px-[8px] cursor-pointer hover:bg-[#f4f5f7] shadow-[0_1px_0_#091e4240]">
         {name}
-        <span className="fa fa-close"></span>
+        <span onClick={(e) => {
+            setCards((prevCards) => {
+                return prevCards.filter((card) => card !== name);
+            })
+        }} className="fa fa-close hover:text-[#172b4d]"></span>
     </div>
 );
 export default Card;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,35 +1,45 @@
 import Card from "./Card";
 import {useState} from "react";
 
-const addCard = (event) => {
-    const btn = event.target;
-    btn.style.display = "none";
-}
-
 const List = ({name}) => {
     const [cards, setCards] = useState([]);
+    const [showCard, setShowCard] = useState(false);
     return (
-        <div className="flex flex-col bg-[#EBECF0] rounded-[3px] py-[2px] px-[8px] pb-[5px] select-none">
-            <div className="font-bold py-[4px] px-[8px] ">
+        <div className="w-[272px] flex flex-col bg-[#EBECF0] rounded-[3px] py-[2px] px-[8px] pb-[5px] select-none">
+            <div className="font-bold py-[4px] px-[8px]">
                 {name}
             </div>
-            {cards.map((curr, index) => <Card key={index}/>)}
-            <div className="">
-                <textarea>
+            {cards.map((curr, index) => <Card key={index} name={curr} setCards={setCards}/>)}
+            {showCard ? <div className="flex flex-col">
+                <textarea required placeholder="Enter a title for this card..."
+                          className="cardLabel px-[8px] overflow-hidden mb-[8px] max-h-[162px] min-h-[54px] p-0 outline-0 text-[14px] shadow-[0_1px_0_#091e4240] rounded-[3px]">
 
                 </textarea>
-                <div>
-                    <button>
+                <div className="flex items-center space-x-4">
+                    <button onClick={(e) => {
+                        const cardLabel = document.querySelector(".cardLabel").value;
+                        if (cardLabel.trim().length === 0)
+                            return;
+                        setShowCard(false);
+                        setCards((prevCards) => {
+                            return [...prevCards, cardLabel];
+                        })
+                    }
+                    } className="bg-[#0079bf] text-white rounded-[3px] py-[6px] px-[12px] hover:bg-[#026aa7]">
                         Add card
                     </button>
-                    <span className="fa fa-close"> </span>
+                    <span onClick={(e) => {
+                        setShowCard(false);
+                    }
+                    } className="fa fa-close text-[#6b778c] text-2xl hover:text-[#172b4d]"> </span>
 
                 </div>
-            </div>
-            <button onClick={(e) => addCard(e)}
-                 className="flex rounded-[3px] text-[#5e6c84] py-[4px] px-[8px] cursor-pointer hover:bg-[#091e4214]">
+            </div> : <button onClick={(e) => {
+                setShowCard(true);
+            }} className="flex rounded-[3px] text-[#5e6c84] py-[4px] px-[8px] cursor-pointer hover:bg-[#091e4214]">
                 <span className="fa-solid fa-plus mt-1 mr-1"></span>Add a card
-            </button>
+            </button>}
+
         </div>
     );
 };
